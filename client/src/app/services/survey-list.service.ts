d47f9a855c3aeda437d6fdcece4f06c29c385b5f
Sauhard <samsauhard1@gmail.com>
2019-04-20 00:50:34 -0400

    Survey

--- client/src/app/services/survey-list.service.ts
+++ client/src/app/services/survey-list.service.ts
@@ -14,7 +14,7 @@ export class SurveyListService {
   private user: User;
   private authToken: any = null;
   private authService: AuthService;
-  private endpoint = 'https://surveyappmean.herokuapp.com/surveys/survey-list/';
+  private endpoint = 'http://localhost:3000/surveys/survey-list/';
 
   //private endpoint = 'https://portfoliomean.herokuapp.com/api/contact-list/';
 
